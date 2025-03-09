d5c27509d91b496f17a8c8c2ec6fb7f47be41a0c
EggToast225 <disposesable328@gmail.com>
2025-03-09 05:28:56 -0400

    Controller has basic CRUD but creating a city as of now yields the cityID being populated, but not cityName; bug is unresolved

---
+++ UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/Controllers/CityController.java
@@ -0,0 +1,57 @@
+package com.example.sql_dbms_ui.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sql_dbms_ui.Models.City;
+import com.example.sql_dbms_ui.repo.CityRepo;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping(value = "/City")
+public class CityController {
+
+    // This handles the sql injection
+    @Autowired
+    private CityRepo cityRepo;
+
+    // Creates new city on city Database
+    @PostMapping (value = "/save")
+    public ResponseEntity<String> saveCity(@RequestBody City city){
+        cityRepo.save(city);
+        return ResponseEntity.ok("saved city");
+    }
+
+    // Gets the city table
+    @GetMapping (value = "/all")
+    public List<City> getCities(){
+        return cityRepo.findAll();
+    }
+
+    // Updates City name
+    @PutMapping (value = "/update/{cityID}")
+    public ResponseEntity<String> updateCity(@PathVariable ("cityID") long cityID, @RequestBody City city){
+        City updatedCity = cityRepo.findById(cityID).get();
+        updatedCity.setCityName(city.getCityName());
+
+        return ResponseEntity.ok("updated city");
+    }
+
+    // Handles HTTP Delete request
+    @DeleteMapping(value = "/delete/{cityID}") 
+    public String deleteEmployee(@PathVariable("cityID") long cityID, @RequestBody City city){
+        City deleteUser = cityRepo.findById(cityID).get();
+        cityRepo.delete(deleteUser);
+        return "deleted user with id: " + cityID;
+    }
+}
