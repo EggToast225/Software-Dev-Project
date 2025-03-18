20c2954997cf6d8bff3aaa12db9eb6af45bb633d
EggToast225 <disposesable328@gmail.com>
2025-03-18 10:34:21 -0400

    Added getter and setter methods and format for phoneNum

--- UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/Models/Demographic.java
+++ UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/Models/Demographic.java
@@ -1,12 +1,15 @@
 package com.example.sql_dbms_ui.Models;
 
-import java.text.SimpleDateFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.NumberFormat;
 
 @Entity
 public class Demographic{
@@ -16,7 +19,6 @@ public class Demographic{
     @JoinColumn(name = "EmpID", referencedColumnName= "EmpID")
     private Employees employee;
 
-
     @Column
     private String gender;
 
@@ -24,9 +26,26 @@ public class Demographic{
     private String race;
 
     @Column
-    private SimpleDateFormat dateOfBirth;
+    private Date dateOfBirth;
 
     @Column
+    @NumberFormat(pattern = "###-###-####")
     private int phoneNum;
+    
+    //getters and setters
 
+    public Employees getEmployee(){return employee;}
+    public void setEmployee(Employees employee){this.employee = employee;}
+
+    public String getGender(){return gender;}
+    public void setGender(String gender){this.gender = gender;}
+
+    public String getRace(){return race;}
+    public void setRace(String race){this.race = race;}
+
+    public Date getDateOfBirth(){return dateOfBirth;}
+    public void setDateOfBirth(Date dateofBirth){this.dateOfBirth = dateofBirth;}
+
+    public int getPhoneNum(){return phoneNum;}
+    public void setPhoneNum(int phoneNum){this.phoneNum = phoneNum;}
 }
