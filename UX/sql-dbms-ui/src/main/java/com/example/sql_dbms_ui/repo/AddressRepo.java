1bd3a40f19d1c350870c1fb826961cc4327fec52
EggToast225 <disposesable328@gmail.com>
2025-03-18 10:45:23 -0400

    removed address repo

--- UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/repo/AddressRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.sql_dbms_ui.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.sql_dbms_ui.Models.Address;
-
-public interface AddressRepo extends JpaRepository<Address, Long>{
-}
