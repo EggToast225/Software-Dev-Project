290a846f5685d3ef5eb27ee9d1d79574ea7f7b87
EggToast225 <disposesable328@gmail.com>
2025-03-18 10:45:07 -0400

    .

--- UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/Models/EmployeeJobTitle.java
+++ UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/Models/EmployeeJobTitle.java
@@ -12,14 +12,14 @@ public class EmployeeJobTitle {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long jobTitleID;
 
-    
     @OneToOne
     @JoinColumn(name = "EmpID") //One employeeID per Employee (Foreign Key)
     private Employees employee;
 
-    @OneToOne()
-
     // getters and setters
     public long getJobTitleID(){return jobTitleID;}
-    public void setJobTitleID(long jobTileID){this.jobTitleID = jobTitleID;}
+    public void setJobTitleID(long jobTitleID){this.jobTitleID = jobTitleID;}
+
+    public Employees getEmployee() {return employee;}
+    public void setEmployee(Employees employee) {this.employee = employee;}
 }
