be1f1cc292ebd4dac9618b225a48d80e9e8794cd
EggToast225 <disposesable328@gmail.com>
2025-03-18 10:31:06 -0400

    .

--- UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/Models/State.java
+++ UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/Models/State.java
@@ -16,11 +16,10 @@ public class State {
     @Column
     private String stateName;
 
-    // Address is a foreign key to State
     //getters and setters
-    public String getStateName(){return stateName;}
-    public void setStateName(String stateName){this.stateName = stateName;}
-
     public long getStateID(){return stateID;}
     public void setStateID(long stateID){this.stateID = stateID;}
+    
+    public String getStateName(){return stateName;}
+    public void setStateName(String stateName){this.stateName = stateName;}
 }
