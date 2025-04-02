d0529e571800b78e30150c11ef22964274f6102f
EggToast225 <disposesable328@gmail.com>
2025-04-02 15:48:19 -0400

    Added custom queries for EmployeesRepo

--- UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/repo/EmployeesRepo.java
+++ UX/sql-dbms-ui/src/main/java/com/example/sql_dbms_ui/repo/EmployeesRepo.java
@@ -1,8 +1,24 @@
 package com.example.sql_dbms_ui.repo;
 
+import java.util.Date;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.sql_dbms_ui.Models.Employees;
 
-public interface EmployeesRepo extends JpaRepository<Employees,Long>{
+
+
+
+
+public interface EmployeesRepo extends JpaRepository<Employees, Long>{
+
+    // Custom Queries
+    Optional<Employees> findBySSN(String ssn);
+    Optional<Employees> findByDOB(Date dob);
+
+    Optional<Employees> findByFirstName(String firstName);
+    Optional<Employees> findByLastName(String lastName);
+
+
 }
